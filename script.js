35f553c1b0b4f0e69b8a9676c18bd3a1d3ef41ee
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-26 22:34:12 -0300

    Dia 26/05

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt("digite seu primeiro nome: ");
+let sobrenome = prompt("digite seu sobrenome: ");
+console.log = (nome + sobrenome);
+let nomeinteiro = (nome + sobrenome);
+
+nomeinteiro.split("da", "de", "do", "dos", "das", "e");
+nomeinteiro.filter("da", "de", "do", "dos", "das", "e");
+
+

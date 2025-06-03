ae9272ea07ad4f1c2f16795ec100bcd619c68515
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-02 22:38:09 -0300

    atualização 02/06

--- teste.js
+++ teste.js
@@ -1,10 +1,7 @@
+const palavaras = "de, da, do"
+
 let nome = prompt("digite seu primeiro nome: ");
 let sobrenome = prompt("digite seu sobrenome: ");
 console.log = (nome + sobrenome);
 let nomeinteiro = (nome + sobrenome);
 
-nomeinteiro.split("da", "de", "do", "dos", "das", "e");
-nomeinteiro.filter("da", "de", "do", "dos", "das", "e");
-
-
-replace("da", "de", "do")
